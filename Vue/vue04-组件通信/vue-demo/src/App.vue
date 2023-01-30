7860bcf8053d90ee6c5abdf8419be021342eb0af
guanh <ghguanh@163.com>
2023-01-30 20:50:58 +0800

    vue组件通信

---
+++ Vue/vue04-组件通信/vue-demo/src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <h3>小卖部</h3>
+    <!-- <son-com price="100" title="篮球" :info="msg"></son-com> -->
+    <son-com  v-for="(item) in list" :key="item.name"
+    :price="item.price"
+    :title="item.name"
+    :pid="item.id"
+    desc="nihao"
+    @cut="cutPrice"></son-com>
+
+  </div>
+</template>
+
+<script>
+import SonCom from './components/son.vue'
+export default {
+  data(){
+        return {
+            list:[
+                {id:1,name:'阿尔卑斯',price:0.5},
+                {id:2,name:'哇哈哈',price:3.5}
+            ]
+        }
+    },
+  components:{
+    SonCom
+  },
+  methods:{
+    cutPrice(num,id){
+      console.log(1)
+      const good = this.list.find(function(el){return el.id === id})
+      good.price -= num
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ Vue/vue04-组件通信/vue-demo/src/components/son.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <div class="goods">
+        <h3>商品：{{title}}</h3>
+        <p>价格：{{price}}</p>
+        <p>描述：{{info}}</p>
+        <p>描述：{{desc}}</p>
+        <button @click="cut(pid)">砍一刀</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+    name:'SonCom',
+    props:['price','title','info','pid','desc'],
+   
+    methods:{
+        cut(id){
+            // this.price -= 1
+            this.$emit('cut',10,id)
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
